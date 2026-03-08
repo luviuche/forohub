c4dbc852d9f29f87a08d00a789aafb9be207528f
lmvm <lmvm.zzz@gmail.com>
2026-03-08 02:00:49 -0500

    Crea el filtro que interceptará las peticiones HTTP

---
+++ src/main/java/com/challenge/forohub/infra/security/SecurityFilter.java
@@ -0,0 +1,45 @@
+package com.challenge.forohub.infra.security;
+
+import com.challenge.forohub.domain.usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Obtenemos el token del header (cabecera) de la petición
+        var authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null) {
+            var token = authHeader.replace("Bearer ", "");
+            var nombreUsuario = tokenService.getSubject(token); // Extraemos el correo
+
+            if (nombreUsuario != null) {
+                // Token válido, forzamos el inicio de sesión en Spring
+                var usuario = usuarioRepository.findByCorreoElectronico(nombreUsuario);
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        // Le decimos al filtro que continúe con el flujo hacia el controlador
+        filterChain.doFilter(request, response);
+    }
+}
