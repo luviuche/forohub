d31adf4f83ebbc3b619c27834f10f86740f990c9
lmvm <lmvm.zzz@gmail.com>
2026-03-08 01:47:53 -0500

    Crea el servicio de fabricación de tokens

---
+++ src/main/java/com/challenge/forohub/infra/security/TokenService.java
@@ -0,0 +1,40 @@
+package com.challenge.forohub.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.challenge.forohub.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    // Traemos la clave secreta desde el application.properties
+    @Value("${api.security.secret}")
+    private String apiSecret;
+
+    public String generarToken(Usuario usuario) {
+        try {
+            // Usamos el algoritmo HMAC256 con nuestra clave secreta
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.create()
+                    .withIssuer("forohub") // Quien emite el token
+                    .withSubject(usuario.getCorreoElectronico()) // A quien le pertenece
+                    .withClaim("id", usuario.getId()) // Guardamos el ID del usuario en el token
+                    .withExpiresAt(generarFechaExpiracion()) // Le damos 2 horas de validez
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error al generar el token JWT", exception);
+        }
+    }
+
+    private Instant generarFechaExpiracion() {
+        // El token expirará en 2 horas. Ajustamos a la zona horaria de tu región (-05:00)
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+    }
+}
