f8a49121cd98eb66b1a41e05a699e96e2c17222f
lmvm <lmvm.zzz@gmail.com>
2026-03-08 01:24:55 -0500

    Actualiza la interfaz UsuarioRepository

--- src/main/java/com/challenge/forohub/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/challenge/forohub/domain/usuario/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package com.challenge.forohub.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    // Spring Boot construye la consulta automáticamente basándose en el nombre del metodo
+    UserDetails findByCorreoElectronico(String correoElectronico);
 }
