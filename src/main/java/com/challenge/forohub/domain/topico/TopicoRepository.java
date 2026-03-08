e6906bd0b8618971e85d42c2bb99172d0e23e637
lmvm <lmvm.zzz@gmail.com>
2026-03-08 00:52:36 -0500

    Actualiza el repositorio para evitar duplicados

--- src/main/java/com/challenge/forohub/domain/topico/TopicoRepository.java
+++ src/main/java/com/challenge/forohub/domain/topico/TopicoRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
     // Spring Boot detecta el nombre del metodo y crea la consulta SQL automáticamente
     boolean existsByTituloAndMensaje(String titulo, String mensaje);
+    // Busca si existe el título y mensaje, pero ignorando el ID del tópico que estamos actualizando
+    boolean existsByTituloAndMensajeAndIdNot(String titulo, String mensaje, Long id);
 }
