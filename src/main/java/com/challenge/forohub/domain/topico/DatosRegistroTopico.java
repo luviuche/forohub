dfbbf633a974802a50505d9e32a8daee93a98ca3
lmvm <lmvm.zzz@gmail.com>
2026-03-07 23:47:45 -0500

    Crea el DTO para validar la entrada

---
+++ src/main/java/com/challenge/forohub/domain/topico/DatosRegistroTopico.java
@@ -0,0 +1,19 @@
+package com.challenge.forohub.domain.topico;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosRegistroTopico(
+        @NotBlank(message = "El título es obligatorio")
+        String titulo,
+
+        @NotBlank(message = "El mensaje es obligatorio")
+        String mensaje,
+
+        @NotNull(message = "El ID del autor es obligatorio")
+        Long autorId,
+
+        @NotNull(message = "El ID del curso es obligatorio")
+        Long cursoId
+) {
+}
