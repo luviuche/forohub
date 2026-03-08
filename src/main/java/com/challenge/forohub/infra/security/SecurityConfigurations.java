b68e1de69fbcf2020065551d4384b633acc385b9
lmvm <lmvm.zzz@gmail.com>
2026-03-08 02:04:00 -0500

    Actualiza la clase SecurityConfigurations

--- src/main/java/com/challenge/forohub/infra/security/SecurityConfigurations.java
+++ src/main/java/com/challenge/forohub/infra/security/SecurityConfigurations.java
@@ -1,23 +1,39 @@
 package com.challenge.forohub.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                // Le decimos a Spring que nuestra API es Stateless (no guarda sesiones)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    // Solo el endpoint de Login es público
+                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    // Todos los demás endpoints requieren autenticación
+                    req.anyRequest().authenticated();
+                })
+                // Añadimos nuestro filtro antes del filtro por defecto de Spring
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
